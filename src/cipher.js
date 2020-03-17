72b428d750078e526d4decc718544820aaa29885
aplaraujo <aplaraujo@gmail.com>
2020-03-17 18:28:26 -0300

    Inclusão da função de cifrar e decifrar mensagens

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,24 @@
-window.cipher = {
-  // ... 
-};
+const encode = (text, shift) => {
+  if (shift < 0) {
+    shift = 26 + (shift % 26);
+  }
+  return (
+    text
+      .split("")
+      .map.call(text, function(char) {
+        let cipherChar = char.charCodeAt(0);
+        if (cipherChar >= 65 && cipherChar <= 90) {
+          return String.fromCharCode((cipherChar - 65 + shift) % 26 + 65);
+        }
+        else if (cipherChar >= 97 && cipherChar <= 122){
+          return String.fromCharCode((cipherChar - 97 + shift) % 26 + 97);
+        }
+        else {
+          return char
+        }
+      })
+      .join("")
+  );
+}
+
+export { encode }
